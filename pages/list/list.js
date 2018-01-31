b2b3f17539aa240438983484d4c8b23812b158c1
gxx <562606139@qq.com>
2018-01-31 10:51:15 +0800

    0131

--- pages/list/list.js
+++ pages/list/list.js
@@ -3,7 +3,44 @@
 const app = getApp()
 Page({
   data: {
+    list:[{
+      latitude: 12,
+      longitude: 12,
+      name: '星巴克门店（营业中）',
+      time: '借款时间：9:00-18:00',
+      address: '浦东新区金高路35',
+      num: '680cm'
+    }, {
+      name: '星巴克门店（营业中）',
+      time: '借款时间：9:00-18:00',
+      address: '浦东新区金高路35',
+      num: '680cm'
+      }, {
+        name: '星巴克门店（营业中）',
+        time: '借款时间：9:00-18:00',
+        address: '浦东新区金高路35',
+        num: '680cm'
+    }, {
+      name: '星巴克门店（营业中）',
+      time: '借款时间：9:00-18:00',
+      address: '浦东新区金高路35',
+      num: '680cm'
+    }]
   },
   onLoad: function () {
   },
+  linkto: function (e){
+    console.log(e)
+    var data = e.currentTarget.dataset.data;
+    var latitude = data.latitude;
+    var longitude = data.longitude;
+    var name = data.name;
+    var address = data.address;
+    wx.openLocation({
+      latitude: latitude,
+      longitude: longitude,
+      name: name,
+      address: address
+    })
+  }
 })
